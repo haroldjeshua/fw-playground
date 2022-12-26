f4846aa1867caa3138da51c4884b8727952ea257
Harold Varde <harvardii15@gmail.com>
2022-12-27 00:13:04 +0800

    MOD: transforming API data

--- weather-app-vanilla/main.js
+++ weather-app-vanilla/main.js
@@ -1,6 +1,16 @@
 import "./style.css";
 import { getWeather } from "./weather";
 
-getWeather(52.52, 13.41, "Asia/Tokyo").then((data) => {
-  console.log(data);
-});
+getWeather(52.52, 13.41, "Asia/Tokyo")
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("Error getting weather");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  // renderDailyWeather(daily);
+  // renderHourly(hourly);
+  document.body.classList.remove("blurred");
+}
