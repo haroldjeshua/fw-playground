f1bd73558967517ba9b93a840eca99170a2b4c35
Harold Varde <harvardii15@gmail.com>
2023-01-14 23:36:50 +0800

    MOD: new logo + svgs

--- mesh-hover-svelte/src/App.svelte
+++ mesh-hover-svelte/src/App.svelte
@@ -26,48 +26,141 @@
 <main>
   <div class="logo-wrapper">
     <svg
-      width="128"
-      height="128"
-      viewBox="0 0 72 72"
+      width="98"
+      height="110"
+      viewBox="0 0 98 110"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect
-        y="2"
-        width="32"
-        height="32"
-        rx="2"
-        fill="#B9A8FB"
-        fill-opacity="0.9"
-      />
-      <path
-        d="M50.2126 3.56023C50.9507 2.09003 53.0493 2.09003 53.7874 3.56023L67.8659 31.6027C68.5336 32.9326 67.5667 34.5 66.0785 34.5H37.9215C36.4333 34.5 35.4664 32.9326 36.1341 31.6027L50.2126 3.56023Z"
-        fill="#7342C6"
-        fill-opacity="0.9"
-      />
-      <path
-        d="M0 38.9697C0 38.4341 0.434148 38 0.969697 38H24C28.4183 38 32 41.5817 32 46C32 50.4183 28.4183 54 24 54H0V38.9697Z"
-        fill="#6C2D87"
-        fill-opacity="0.9"
-      />
-      <path
-        d="M0 54H16L31.5302 67.5889C31.8287 67.8502 32 68.2276 32 68.6243C32 69.3841 31.3841 70 30.6243 70H2C0.895431 70 0 69.1046 0 68V54Z"
-        fill="#6C2D87"
-        fill-opacity="0.9"
-      />
-      <path
-        d="M53.7874 68.4398C53.0493 69.91 50.9507 69.91 50.2126 68.4398L36.1341 40.3973C35.4664 39.0674 36.4333 37.5 37.9215 37.5L66.0785 37.5C67.5667 37.5 68.5336 39.0674 67.8659 40.3973L53.7874 68.4398Z"
-        fill="#6C4DE6"
-        fill-opacity="0.9"
-      />
+      <g style="mix-blend-mode:luminosity">
+        <path
+          d="M0 15.2422C0 11.471 0 9.58533 1.17157 8.41376C2.34315 7.24219 4.22876 7.24219 8 7.24219H34.9347C38.706 7.24219 40.5916 7.24219 41.7632 8.41376C42.9347 9.58533 42.9347 11.471 42.9347 15.2422V42.1769C42.9347 45.9482 42.9347 47.8338 41.7632 49.0053C40.5916 50.1769 38.706 50.1769 34.9347 50.1769H8.00001C4.22877 50.1769 2.34315 50.1769 1.17157 49.0053C0 47.8338 0 45.9482 0 42.1769V15.2422Z"
+          fill="url(#paint0_linear_1067_258)"
+        />
+        <path
+          d="M92.0863 38.8163C94.2983 43.873 95.4043 46.4014 94.2201 48.2119C93.0359 50.0225 90.2762 50.0225 84.7568 50.0225L65.1408 50.0225C59.5851 50.0225 56.8072 50.0225 55.6244 48.2005C54.4416 46.3786 55.5722 43.8412 57.8334 38.7665L67.7317 16.5516C70.8972 9.44725 72.48 5.89508 75.0865 5.90395C77.693 5.91282 79.2515 9.47568 82.3686 16.6014L92.0863 38.8163Z"
+          fill="url(#paint1_linear_1067_258)"
+        />
+        <path
+          d="M0 62.5871C0 61.1923 1.13074 60.0615 2.52557 60.0615H32.201C38.1291 60.0615 42.9347 64.8672 42.9347 70.7952C42.9347 76.7233 38.1291 81.5289 32.201 81.5289H0V62.5871Z"
+          fill="url(#paint2_linear_1067_258)"
+        />
+        <path
+          d="M0 81.5293H21.4674L42.3044 99.7617C42.705 100.112 42.9347 100.619 42.9347 101.151C42.9347 102.17 42.1083 102.997 41.0889 102.997H8.00001C4.22877 102.997 2.34315 102.997 1.17157 101.825C0 100.654 0 98.7679 0 94.9967V81.5293Z"
+          fill="url(#paint3_linear_1067_258)"
+        />
+        <path
+          d="M58.1346 71.2362C55.9199 66.1785 54.8126 63.6496 55.9967 61.8385C57.1808 60.0273 59.9415 60.0273 65.4628 60.0273H85.0691C90.6268 60.0273 93.4056 60.0273 94.5884 61.8499C95.7711 63.6724 94.6392 66.2103 92.3753 71.286L82.4819 93.468C79.3165 100.565 77.7338 104.114 75.1284 104.105C72.5229 104.096 70.9644 100.537 67.8474 93.4182L58.1346 71.2362Z"
+          fill="url(#paint4_linear_1067_258)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_1067_258"
+          x1="3.00352e-07"
+          y1="50"
+          x2="43"
+          y2="7"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DCDCDC" />
+          <stop offset="1" stop-color="#888888" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1067_258"
+          x1="53"
+          y1="50"
+          x2="97"
+          y2="-9.52893e-07"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DCDCDC" />
+          <stop offset="1" stop-color="#888888" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_1067_258"
+          x1="21"
+          y1="82"
+          x2="43"
+          y2="60.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DCDCDC" />
+          <stop offset="1" stop-color="#888888" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_1067_258"
+          x1="21"
+          y1="82"
+          x2="43"
+          y2="61"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DCDCDC" />
+          <stop offset="1" stop-color="#B09CDA" />
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_1067_258"
+          x1="53"
+          y1="110"
+          x2="97"
+          y2="60"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DCDCDC" />
+          <stop offset="1" stop-color="#888888" />
+        </linearGradient>
+      </defs>
     </svg>
   </div>
-  <h1>HARV<small>.io</small></h1>
-  <h3>Frontend + Design</h3>
+
+  <h1>Harold Varde</h1>
+  <h3>Frontend & Design</h3>
   <p>
     A frontend developer with a strong eye for design. Building apps with
     intuitive interfaces on the web.
   </p>
+
+  <button>
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1067_170)">
+        <path
+          d="M2.66602 8.66699C3.85474 8.80826 4.96138 9.34535 5.80785 10.1918C6.65433 11.0383 7.19141 12.1449 7.33268 13.3337C7.92196 12.9939 8.41488 12.5095 8.76483 11.9263C9.11479 11.343 9.31023 10.6801 9.33268 10.0003C10.4521 9.60653 11.4296 8.88965 12.1416 7.94033C12.8536 6.99101 13.2681 5.85188 13.3327 4.66699C13.3327 4.13656 13.122 3.62785 12.7469 3.25278C12.3718 2.87771 11.8631 2.66699 11.3327 2.66699C10.1478 2.7316 9.00866 3.1461 8.05934 3.85809C7.11002 4.57008 6.39314 5.54759 5.99935 6.66699C5.31952 6.68944 4.65665 6.88488 4.07339 7.23484C3.49013 7.5848 3.00574 8.07771 2.66602 8.66699Z"
+          stroke="#121212"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4.66634 9.33301C3.97249 9.72472 3.41144 10.3147 3.05511 11.0274C2.69877 11.7401 2.5634 12.5429 2.66634 13.333C3.45644 13.4359 4.25929 13.3006 4.97195 12.9442C5.68462 12.5879 6.27462 12.0269 6.66634 11.333"
+          stroke="#121212"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M10.0007 6.66634C10.3688 6.66634 10.6673 6.36786 10.6673 5.99967C10.6673 5.63148 10.3688 5.33301 10.0007 5.33301C9.63246 5.33301 9.33398 5.63148 9.33398 5.99967C9.33398 6.36786 9.63246 6.66634 10.0007 6.66634Z"
+          stroke="#121212"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1067_170">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+
+    Explore
+  </button>
 </main>
 
 <img src={rain} alt="noise" />
@@ -209,14 +302,12 @@
   h1 {
     font-family: "Krona One", sans-serif;
     font-size: clamp(4vw, 4rem, 10vw);
-    margin: 0;
-  }
-
-  h1 > small {
-    font-size: calc(clamp(4vw, 4rem, 10vw) / 4);
+    text-transform: uppercase;
+    margin: 0 0 1rem 0;
   }
 
   h3 {
+    font-size: clamp(2vw, 1.5rem, 5vw);
     font-weight: 400;
     margin: 0;
   }
