78f016adc3b2ee1cfe040ed68eca6478bbcd0040
Harold Varde <harvardii15@gmail.com>
2023-01-18 01:23:15 +0800

    ADD: markup styling + extend

--- lynx/pages/index.tsx
+++ lynx/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import data from "../data.json";
+import { FiExternalLink } from "react-icons/fi";
 
 function LinkCard({
   url,
@@ -13,11 +14,26 @@ function LinkCard({
   return (
     <a
       href={url}
-      className="flex items-center p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+      className="w-full flex items-center mb-4 p-3 border border-gray-300 rounded-md hover:scale-102 transition-all"
     >
-      <div className="font-bold text-lg">
-        <div className="flex flex-col">
-          <h1 className="font-bold text-lg">{title}</h1>
+      <div className="flex justify-between items-center gap-1 w-full">
+        <div className="flex">
+          {image && (
+            <Image
+              className="rounded-sm"
+              alt={title}
+              src={image}
+              width={32}
+              height={32}
+            />
+          )}
+        </div>
+        <div className="flex justify-center
+        ">
+          <h2 className="font-semibold w-full text-center">{title}</h2>
+        </div>
+        <div className="flex">
+            <FiExternalLink/>
         </div>
       </div>
     </a>
@@ -26,7 +42,7 @@ function LinkCard({
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center mt-16 w-full">
+    <section className="w-full flex flex-col items-center justify-center mt-16 px-8">
       <Image
         className="rounded-full"
         alt={data.name}
@@ -34,7 +50,7 @@ export default function Home() {
         width={96}
         height={96}
       />
-      <h1 className="font-bold mt-4 text-2xl">{data.name}</h1>
+      <h1 className="font-bold mt-4 mb-8 text-2xl">{data.name}</h1>
       {data.links.map((link) => (
         <LinkCard key={link.url} {...link} />
       ))}
--- lynx/tailwind.config.js
+++ lynx/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        102: "1.025",
+      },
+    },
   },
   plugins: [],
 };
