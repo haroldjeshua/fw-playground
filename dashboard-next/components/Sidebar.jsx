23d31780dc366cad3af8bea79a69b4506629a835
Harold Varde <harvardii15@gmail.com>
2023-01-19 21:58:20 +0800

    MOD: sidebar

--- dashboard-next/components/Sidebar.jsx
+++ dashboard-next/components/Sidebar.jsx
@@ -1,11 +1,45 @@
+import Link from "next/link";
 import React from "react";
+import { FiBox } from "react-icons/fi";
 
 const Sidebar = ({ children }) => {
   return (
-    <div className="flex">
-      <h1>Sidebar</h1>
-      <div className="fixed w-20 h-screen p-4 flex flex-col justify-between bg-white border-r-[1px]"></div>
-      <main className="w-full ml-20">{children}</main>
+    <div className="flex m-0 p-0">
+      <div className="fixed top-[0.25rem] left-[0.25rem] w-[80px] h-[calc(100vh_-_0.5rem)] p-4 flex flex-col justify-between bg-purple-800 text-white rounded-2xl">
+        <div className="flex flex-col items-center">
+          <Link href="/">
+            <div className="bg-white text-purple-800 p-3 rounded-lg inline-block">
+              <FiBox size={32} />
+            </div>
+          </Link>
+          <div
+            id="dashboardMenu"
+            className="flex flex-col gap-2 mt-2 py-2 border-t-[1px]"
+          >
+            <Link href="/">
+              <div className="bg-purple-800 p-3 rounded-lg inline-block hover:bg-purple-700 transition-all ease">
+                <FiBox size={32} />
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="bg-purple-800 p-3 rounded-lg inline-block hover:bg-purple-700 transition-all ease">
+                <FiBox size={32} />
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="bg-purple-800 p-3 rounded-lg inline-block hover:bg-purple-700 transition-all ease">
+                <FiBox size={32} />
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="bg-purple-800 p-3 rounded-lg inline-block hover:bg-purple-700 transition-all ease">
+                <FiBox size={32} />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <main className="w-full ml-[calc(80px_+_0.25rem)]">{children}</main>
     </div>
   );
 };
