046d535676126cdb1f4833a69eb2ac10a1c97562
Harold Varde <harvardii15@gmail.com>
2023-01-19 16:45:25 +0800

    ADD: app head

---
+++ lynx/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Harv Lynx</title>
+        <meta name="description" content="single page link management app with Next.js + TailwindCSS" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </>
+    );
+  }
