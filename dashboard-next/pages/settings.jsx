4aad4a238d7e4af6fdfc0515b028f962a1c011f6
Harold Varde <harvardii15@gmail.com>
2023-01-20 23:45:30 +0800

    DONE

--- dashboard-next/pages/settings.jsx
+++ dashboard-next/pages/settings.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Link from "next/link";
+import { FiHome } from "react-icons/fi";
 
 const settings = () => {
-  return <div>settings</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 w-full h-screen">
+      <img src="/loading.svg" alt="/" className="w-[256px]" />
+      <h2>Under construction</h2>
+      <Link
+        href="/"
+        className="flex items-center gap-2 bg-purple-200 text-purple-800 font-semibold p-4 rounded-lg hover:bg-purple-300 transition-all ease"
+      >
+        <FiHome /> Back to Dashboard
+      </Link>
+    </div>
+  );
 };
 
 export default settings;
