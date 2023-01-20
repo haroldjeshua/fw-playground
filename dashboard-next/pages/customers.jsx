48ecc950ee1df451d92df5199af21686de4e71a3
Harold Varde <harvardii15@gmail.com>
2023-01-20 22:53:50 +0800

    Automated commit, build and deploy

--- dashboard-next/pages/customers.jsx
+++ dashboard-next/pages/customers.jsx
@@ -1,4 +1,4 @@
-import { FiMoreVertical } from "react-icons/fi";
+import { FiUser, FiMoreVertical } from "react-icons/fi";
 import data from "../data.json";
 
 const customers = () => {
@@ -12,10 +12,24 @@ const customers = () => {
         <div className="w-full bg-white m-auto p-4 border rounded-lg overflow-y-auto">
           <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-col-s2 items-center justify-between my-4 p-2 cursor-pointer">
             <span>Name</span>
-            <span>Email</span>
-            <span>Last Order</span>
-            <span>Payment Method</span>
+            <span className="text-right sm:text-left">Email</span>
+            <span className="hidden md:grid">Last Order</span>
+            <span className="hidden sm:grid">Payment Method</span>
           </div>
+          <ul>
+            {data.orders.map((order, id) => (
+              <li
+                key={id}
+                className="grid grid-cols-2 md:grid-cols-3 items-center justify-between bg-gray-50 hover:bg-gray-200 my-4 p-2 rounded-lg cursor-pointer"
+              >
+                <div>
+                  <div>
+                    <FiUser />
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
