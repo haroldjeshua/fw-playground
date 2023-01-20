e7d2bfc12af5610d6c4cb1a541cb0d804d643c77
Harold Varde <harvardii15@gmail.com>
2023-01-20 21:42:37 +0800

    Automated commit, build and deploy

--- dashboard-next/components/BarChart.jsx
+++ dashboard-next/components/BarChart.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js/auto";
+
+ChartJS.register(CategoryScale, LinearScale, Title, Tooltip, Legend);
 
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+      datasets: [
+        {
+          label: "Revenue $",
+          data: [1212, 3442, 4654, 4234, 6124, 5444, 1235],
+          borderColor: "hsl(211, 76%, 45%)",
+          backgroundColor: "hsla(211, 76%, 45%, 50%)",
+        },
+      ],
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
 };
 
 export default BarChart;
--- dashboard-next/components/TopCards.jsx
+++ dashboard-next/components/TopCards.jsx
@@ -5,6 +5,7 @@ const TopCards = () => {
     <div className="grid lg:grid-cols-5 gap-4 p-4">
       {data.cards.map((card, index) => (
         <div
+          key={card.title}
           className={`${
             index === data.cards.length - 1 ? "lg:col-span-1" : "lg:col-span-2"
           } col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg`}
