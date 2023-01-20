3204c47984788518f5daf6a695799613adc157eb
Harold Varde <harvardii15@gmail.com>
2023-01-20 18:01:53 +0800

    MOD: topcards data

--- dashboard-next/components/TopCards.jsx
+++ dashboard-next/components/TopCards.jsx
@@ -1,35 +1,21 @@
-import React from "react";
+import data from "../data.json";
 
 const TopCards = () => {
   return (
-    <div className="grid lg:grid-cols-5 gap-4 p-4">
-      <div className="lg:col-span-2 col-span-1 bg-white justify-between w-full border p-4 rounded-lg">
-        <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">Card Amount</p>
-          <p className="text-gray-600">Card Text</p>
+    <div className="grid lg:grid-cols-6 gap-4 p-4">
+      {data.cards.map((card) => (
+        <div className="lg:col-span-2 col-span-1 bg-white justify-between w-full border p-4 rounded-lg">
+          <div className="flex flex-col w-full pb-4">
+            <p className="text-2xl font-bold">{card.title}</p>
+            <p className="text-gray-600">
+              {card.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </p>
+          </div>
+          <p className="flex justify-center items-center bg-green-200  p-2 rounded-lg">
+            <span className="text-green-700 font-semibold">{card.gains}</span>
+          </p>
         </div>
-        <p className="flex justify-center items-center bg-green-200  p-2 rounded-lg">
-          <span className="text-green-700 font-semibold">+18%</span>
-        </p>
-      </div>
-      <div className="lg:col-span-2 col-span-1 bg-white justify-between w-full border p-4 rounded-lg">
-        <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">Card Amount</p>
-          <p className="text-gray-600">Card Text</p>
-        </div>
-        <p className="flex justify-center items-center bg-green-200  p-2 rounded-lg">
-          <span className="text-green-700 font-semibold">+18%</span>
-        </p>
-      </div>
-      <div className="bg-white justify-between w-full border p-4 rounded-lg">
-        <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">Card Amount</p>
-          <p className="text-gray-600">Card Text</p>
-        </div>
-        <p className="flex justify-center items-center bg-green-200  p-2 rounded-lg">
-          <span className="text-green-700 font-semibold">+18%</span>
-        </p>
-      </div>
+      ))}
     </div>
   );
 };
--- dashboard-next/pages/index.js
+++ dashboard-next/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen bg-gray-100">
+      <main className="min-h-screen bg-gray-100 font-manrope">
         <Header />
         <TopCards />
       </main>
