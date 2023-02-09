1bab02eaa64cc2a7b657ecdd0d6fb6a9d29d5805
Harold Varde <harvardii15@gmail.com>
2023-02-09 13:22:16 +0800

    ADD:  add essential folders + nav comp

---
+++ adv-shopcart-react-ts/README.md
@@ -0,0 +1,12 @@
+# Advanced Shopping Cart with React and Typescript
+
+uses: react-bootstrap, react-router-dom
+
+Notes:
+
+- components
+- context
+- data
+- hooks
+- pages
+- utils
---
+++ adv-shopcart-react-ts/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to={"/"}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={"/store"}>
+            Store
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={"/about"}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-shopping-bag"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M6.331 8h11.339a2 2 0 0 1 1.977 2.304l-1.255 8.152a3 3 0 0 1 -2.966 2.544h-6.852a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304z"></path>
+            <path d="M9 11v-5a3 3 0 0 1 6 0v5"></path>
+          </svg>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+}
