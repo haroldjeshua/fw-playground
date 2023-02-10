8287d14cc1d066e54af24df32292ff0dc0f2f275
Harold Varde <harvardii15@gmail.com>
2023-02-10 21:41:39 +0800

    MOD:  shopcart context -> buttons actions

--- adv-shopcart-react-ts/src/context/ShoppingCartContext.tsx
+++ adv-shopcart-react-ts/src/context/ShoppingCartContext.tsx
@@ -6,7 +6,6 @@ type ShoppingCartProviderProps = {
 
 type CartItem = {
   id: number;
-  name: string;
   quantity: number;
 };
 
@@ -46,8 +45,37 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     });
   }
 
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity == 1) {
+        return currentItems.filter((item) => item.id !== id);
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{ getItemQuantity }}>
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
