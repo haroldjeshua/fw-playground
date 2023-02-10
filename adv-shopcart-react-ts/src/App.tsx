23415816fb572047ab77d5a14087bf4a474096c9
Harold Varde <harvardii15@gmail.com>
2023-02-10 21:13:09 +0800

    MOD:  shopcart context + func

--- adv-shopcart-react-ts/src/App.tsx
+++ adv-shopcart-react-ts/src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import "./App.css";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import "./App.css";
 
 function App() {
   return (
--- adv-shopcart-react-ts/src/context/ShoppingCartContext.tsx
+++ adv-shopcart-react-ts/src/context/ShoppingCartContext.tsx
@@ -1,18 +1,53 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type ShoppingCartProviderProps = {
   children: ReactNode;
 };
 
-const ShoppingCartContext = createContext({});
+type CartItem = {
+  id: number;
+  name: string;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider value={{ getItemQuantity }}>
       {children}
     </ShoppingCartContext.Provider>
   );
