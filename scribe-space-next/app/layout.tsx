102d36d0392c5d1b9b54cc31f2b716b88af5d569
Harold Varde <harvardii15@gmail.com>
2023-02-13 16:56:03 +0800

    ADD:  tw + config + header styles

--- scribe-space-next/app/layout.tsx
+++ scribe-space-next/app/layout.tsx
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import { Inter } from "@next/font/google";
+import { IconSquareRoundedLetterS } from "@tabler/icons-react";
+import "styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -6,13 +11,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const header = (
-    <header>
-      <div>
-        <Link href="/">
-          <h1>Scribe Space</h1>
-        </Link>
-        <p>Welcome scribers.</p>
-      </div>
+    <header className="flex flex-col justify-center text-center mb-8">
+      <IconSquareRoundedLetterS size={64} />
+      <Link href="/">
+        <h1 className="text-3xl font-semibold">Scribe Space</h1>
+      </Link>
+      <h4 className="text-lg font-medium">
+        A Digital Journey to Productivity and Efficiency
+      </h4>
+
+      <p className="text-md">
+        Welcome to Scribe Space, your one-stop destination for all things tech
+        life.
+      </p>
     </header>
   );
 
@@ -27,10 +38,14 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>
-        {header}
-        {children}
-        {footer}
+      <body className="relative flex min-h-screen flex-col justify-start overflow-hidden bg-gray-100 py-24">
+        <main
+          className={`${inter.className} relative w-full bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-2xl sm:rounded-lg sm:px-10`}
+        >
+          {header}
+          {children}
+          {footer}
+        </main>
       </body>
     </html>
   );
