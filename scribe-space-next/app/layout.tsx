f3e4ba45e68806a2105ae752d6539ece08cd498a
Harold Varde <harvardii15@gmail.com>
2023-02-13 18:04:51 +0800

    MOD:  post styles

--- scribe-space-next/app/layout.tsx
+++ scribe-space-next/app/layout.tsx
@@ -11,11 +11,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const header = (
-    <header className="flex flex-col items-center text-center mb-8">
+    <header className="flex flex-col items-center text-center">
       <IconSquareRoundedLetterS size={64} />
       <Link href="/">
         <h1 className="text-3xl font-semibold">Scribe Space</h1>
       </Link>
+      <h6 className="text-sm font-semibold mt-2 mb-4">
+        by&nbsp;
+        <Link
+          href="https://github.com/haroldjeshua"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          @harv
+        </Link>{" "}
+      </h6>
       <h4 className="text-lg font-medium">
         A Digital Journey to Productivity and Efficiency
       </h4>
@@ -28,8 +38,17 @@ export default function RootLayout({
   );
 
   const footer = (
-    <footer className="relative bottom-0">
-      <h4>Made with ❤ by Harv</h4>
+    <footer className="flex justify-center mt-auto">
+      <h4>
+        Made with ❤ by&nbsp;
+        <Link
+          href="https://github.com/haroldjeshua"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          @harv
+        </Link>
+      </h4>
     </footer>
   );
 
@@ -40,7 +59,7 @@ export default function RootLayout({
         className={`${inter.className} relative flex flex-col min-h-screen justify-start bg-gradient-to-br from-indigo-200 to-violet-300 py-16`}
       >
         <main
-          className={`flex flex-col gap-4 relative w-full bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-2xl sm:rounded-lg sm:px-10`}
+          className={`flex flex-col gap-8 relative w-full bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-2xl sm:rounded-lg sm:px-10`}
         >
           {header}
           {children}
--- scribe-space-next/components/PostPreview.tsx
+++ scribe-space-next/components/PostPreview.tsx
@@ -3,11 +3,11 @@ import { PostMetadata } from "./PostMetadata";
 
 const PostPreview = (props: PostMetadata) => {
   return (
-    <article className="border border-inherit-400 p-4 rounded-lg">
+    <article className="flex flex-col gap-2 border-2 border-inherit p-4 rounded-lg cursor-pointer hover:border-[currentColor] transition-all ease-linear">
       <Link href={`/posts/${props.slug}`}>
-        <h2>{props.title}</h2>
+        <h2 className="text-xl font-medium leading-6">{props.title}</h2>
       </Link>
-      <p>{props.date}</p>
+      <p className="text-sm font-light mt-auto">{props.date}</p>
     </article>
   );
 };
