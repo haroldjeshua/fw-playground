d458371ca90d5a2b362e94d40b4b34ccda26ba7d
Harold Varde <harvardii15@gmail.com>
2023-02-13 15:32:39 +0800

    ADD:  post preview

--- scribe-space-next/app/page.tsx
+++ scribe-space-next/app/page.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import getPostMetaData from "@/components/getPostMetaData";
+import PostPreview from "@/components/PostPreview";
 
 const HomePage = () => {
   const postMetaData = getPostMetaData();
   const postPreviews = postMetaData.map((post) => (
-    <Link href={`/posts/${post.slug}`}>
-      <h2>{post.title}</h2>
-      <p>{post.date}</p>
-    </Link>
+    <PostPreview key={post.slug} {...post} />
   ));
   return <div>{postPreviews}</div>;
 };
