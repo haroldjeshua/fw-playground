d584c0dae3b0e56c082055b7152831867f50491e
Harold Varde <harvardii15@gmail.com>
2023-03-14 18:48:35 +0800

    MOD: proj11 -> routing

--- downnotes-react/src/App.tsx
+++ downnotes-react/src/App.tsx
@@ -1,11 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-      <h1>Downnotes App</h1>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>}></Route>
+      <Route path="/new" element={<h1>New</h1>}></Route>
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>}></Route>
+        <Route path="edit" element={<h1>Edit</h1>}></Route>
+      </Route>
+      <Route path="*" element={<Navigate to="/" />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
