8715c4fb60cc46e4ccabe1126d48f6b28a244075
Harold Varde <harvardii15@gmail.com>
2023-03-15 13:10:19 +0800

    MOD: proj11 -> localstorage hook + create note logic

--- downnotes-react/src/App.tsx
+++ downnotes-react/src/App.tsx
@@ -1,11 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useMemo } from "react";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
 
-type Note = {
+export type Note = {
   id: string;
 } & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
 
 export type NoteData = {
   title: string;
@@ -19,6 +32,24 @@ export type Tag = {
 };
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<RawNote[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote(data: NoteData) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, { ...data, id: uuidV4() }];
+    });
+  }
+
   return (
     <Container className="my-4">
       <Routes>
