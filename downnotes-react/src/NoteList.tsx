961d226c1caf259770ee911340b63fa8e993dd7b
Harold Varde <harvardii15@gmail.com>
2023-03-16 15:55:46 +0800

    MOD: proj11 -> note list styles

--- downnotes-react/src/NoteList.tsx
+++ downnotes-react/src/NoteList.tsx
@@ -1,8 +1,9 @@
 import { useMemo, useState } from "react";
-import { Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "./App";
+import styles from "./NoteList.module.css";
 
 type SimplifiedNote = {
   id: string;
@@ -82,7 +83,7 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
           </Col>
         </Row>
       </Form>
-      <Row xs={1} sm={2} lg={4} classNameg-3>
+      <Row xs={1} sm={2} lg={4} className="g-3">
         {filteredNotes.map((note) => (
           <Col key={note.id}>
             <NoteCard id={note.id} title={note.title} tags={note.tags} />
@@ -100,7 +101,27 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
       to={`/${id}`}
       className={`h-100 text-reset text-decoration-none ${styles.card}`}
     >
-      <Card.Body>Hello</Card.Body>
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge className="text-truncate" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
     </Card>
   );
 }
